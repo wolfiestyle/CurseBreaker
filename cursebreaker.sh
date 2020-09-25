bfef2f9d40318568853224e29c6194b0f13550c9
wolfiestyle <slayerbeast@gmail.com>
2020-09-25 13:54:14 -0300

    use `CURSEBREAKER_PATH` env variable to set the game path

--- cursebreaker.sh
+++ cursebreaker.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -eu
-WOWPATH="$HOME/Games/world-of-warcraft/drive_c/Blizzard/World of Warcraft/_retail_"
+export CURSEBREAKER_PATH="$HOME/Games/world-of-warcraft/drive_c/Blizzard/World of Warcraft/_retail_"
 
 DIRNAME=$(dirname `readlink -f "$0"`)
-cd "$WOWPATH"
 exec "$DIRNAME/CurseBreaker.py"
