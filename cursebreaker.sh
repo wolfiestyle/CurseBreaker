f0693c7dfc7258e56a258042169589a02b9acec9
wolfiestyle <slayerbeast@gmail.com>
2021-05-03 15:23:37 -0400

    added classic support to the launcher script

--- cursebreaker.sh
+++ cursebreaker.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-set -eu
-export CURSEBREAKER_PATH="$HOME/Games/world-of-warcraft/drive_c/Blizzard/World of Warcraft/_retail_"
+case $1 in
+    classic)
+        EXPAC=_classic_
+        ;;
+    *)
+        EXPAC=_retail_
+        ;;
+esac
+
+export CURSEBREAKER_PATH="$HOME/Games/world-of-warcraft/drive_c/Blizzard/World of Warcraft/$EXPAC"
 
 DIRNAME=$(dirname `readlink -f "$0"`)
 exec "$DIRNAME/CurseBreaker.py"
